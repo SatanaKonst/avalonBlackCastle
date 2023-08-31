ef06fcf105b9cfe535f78179197c487216bce2de
Konstantin <Satana.Konst@gmail.com>
2023-08-31 16:05:56 +0300

    ДОбавлено управление монстрами и локациями

--- src/src/components/Editor.vue
+++ src/src/components/Editor.vue
@@ -1,42 +1,10 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-8 border">
-        <div id="graph"></div>
-      </div>
-      <div class="col-4">
-        <h1>Меню управления</h1>
-        <button class="btn btn-info mb-3" type="button" @click="addNode(null)">Добавить локацию</button>
+  <div class="container mb-5">
+    <!--    Локации-->
+    <div class="row mb-5">
+      <!--Управление локациями-->
+      <div class="col-3">
         <div class="row">
-          <div class="col-12 mb-5" v-if="selectedNode!==null">
-            <h2>Настройки локации</h2>
-
-            <div class="row">
-              <div class="col-12">
-                <input type="text" class="form-control mb-1" v-model="selectedNode.data.name">
-              </div>
-              <div class="col-12">
-                <textarea class="w-100 bg-white text-body" v-model="selectedNode.data.description"></textarea>
-              </div>
-            </div>
-
-            <div class="row">
-              <div class="col-12">
-                <h3>Связь</h3>
-                <select class="form-control mb-2" multiple @change="addLink($event, null)">
-                  <option v-for="link in linksNodes"
-                          :value="link.id"
-                          v-if="link.id!==selectedNode.id"
-                          :key="link.id">{{ link.name }}
-                  </option>
-                </select>
-              </div>
-            </div>
-            <button class="btn btn-success" type="button" @click="updateNode()">
-              <i class="fa-solid fa-save"> Сохранить</i>
-            </button>
-          </div>
-
           <div class="col-12">
             <h2 class="d-inline">Локации</h2>
             <button class="btn btn-sm btn-danger" v-if="nodes.length>0" @click="clear">
@@ -48,17 +16,21 @@
                   v-for="(node,nodeIndex) in nodes"
                   :key="nodeIndex">
                 {{ node.data.name }}
-                <button class="btn btn-success float-end" type="button" @click="selectedNode=nodes[nodeIndex]">
-                  <i class="fa-solid fa-pen-to-square"></i>
-                </button>
 
-                <button class="btn btn-danger float-end" type="button" @click="removeNode(nodeIndex)">
-                  <i class="fa-solid fa-trash"></i>
-                </button>
+                <div class="float-end">
+                  <button class="btn btn-success" type="button" @click="selectedNode=nodes[nodeIndex]">
+                    <i class="fa-solid fa-pen-to-square"></i>
+                  </button>
+
+                  <button class="btn btn-danger" type="button" @click="removeNode(nodeIndex)">
+                    <i class="fa-solid fa-trash"></i>
+                  </button>
+                </div>
               </li>
             </ul>
           </div>
-
+        </div>
+        <div class="row">
           <div class="col-12 mt-3">
             <label v-if="nodes.length===0">
               Импорт
@@ -67,18 +39,216 @@
                      @change="importExportFile"
                      value="Импорт">
             </label>
-            <div v-if="nodes.length>0">
+            <button class="btn btn-info" type="button" @click="addNode(null)">
+              <i class="fa-solid fa-plus"></i>
+            </button>
+            <template v-if="nodes.length>0">
               <button class="btn btn-success" type="button" @click="save()">
-                <i class="fa-solid fa-save">Сохранить граф</i>
+                <i class="fa-solid fa-save">Сохранить</i>
               </button>
               <button class="btn btn-success" type="button" @click="exportNodes()">
                 <i class="fa-solid fa-save">Экспорт</i>
               </button>
-            </div>
+            </template>
           </div>
         </div>
       </div>
+      <!--      Граф-->
+      <div class="col-4 border">
+        <div id="graph"></div>
+      </div>
+      <!--      насройка локации-->
+      <div class="col-5">
+        <h2>Настройки локации</h2>
+        <div class="row">
+          <div class="col-12 mb-5" v-if="selectedNode!==null">
+            <div class="row">
+              <div class="col-12">
+                <input type="text" class="form-control mb-1" v-model="selectedNode.data.name">
+              </div>
+              <div class="col-12">
+                <textarea class="w-100 bg-white text-body"
+                          v-model="selectedNode.data.description"
+                          placeholder="Описание локации"></textarea>
+              </div>
+
+              <!--              Связи-->
+              <div class="row">
+                <div class="col-12">
+                  <h3>Связь</h3>
+                  <select class="form-control mb-2" multiple @change="addLink($event, null)">
+                    <option v-for="link in linksNodes"
+                            :value="link.id"
+                            v-if="link.id!==selectedNode.id"
+                            :key="link.id">{{ link.name }}
+                    </option>
+                  </select>
+                </div>
+              </div>
+
+              <!--              ресурсы-->
+              <div class="col-12 mb-4 border-bottom pb-2">
+                <h3>Ресурсы</h3>
+                <div class="row" v-for="(resourceItems, resourceKey) in selectedNode.data.resource" :key="resourceKey">
+                  <div class="col-12">
+                    <h5 class="text-end">
+                      {{ resourceKey }}
+                      <i class="fa"
+                         v-bind:class="{'fa-images': resourceKey==='images' , 'fa-file-audio': resourceKey==='audio', 'fa-video': resourceKey==='video'}"></i>
+                    </h5>
+                    <div class="row" v-for="(item, index) in resourceItems"
+                         :key="resourceKey+' '+index">
+                      <div class="col-8">
+                        <input type="text"
+                               class="form-control mb-2"
+                               v-model="selectedNode.data.resource[resourceKey][index]">
+                      </div>
+                      <div class="col-4">
+                        <button class="btn btn-danger btn-sm"
+                                @click="selectedNode.data.resource[resourceKey].splice(index, 1)">
+                          <i class="fa fa-trash"></i>
+                        </button>
+                      </div>
+                    </div>
+
+                    <button class="btn btn-success btn-sm d-block"
+                            @click="selectedNode.data.resource[resourceKey].push('')">
+                      <i class="fa fa-plus-circle"></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <button class="btn btn-success" type="button" @click="updateNode()">
+              <i class="fa-solid fa-save"> Сохранить</i>
+            </button>
+          </div>
+
+        </div>
+      </div>
     </div>
+
+    <!--    Монстрятина-->
+    <div class="row">
+      <div class="col-12">
+        <h2 class="d-inline-block">Монстры</h2>
+        <button class="btn btn-success btn-sm" @click="addMonster(null)">
+          <i class="fa fa-plus-circle"></i>
+        </button>
+
+        <div class="accordion" id="monsterAccordion">
+          <div class="card" v-for="(monster, monsterIndex) in monsters" :key="monsterIndex">
+            <div class="card-header" :id="'headingMonster'+monsterIndex">
+              <h5 class="mb-0">
+                <button class="btn btn-link" type="button" @click="showCollapse('collapseMonster'+monsterIndex)">
+                  {{ monster.name }}
+                </button>
+              </h5>
+            </div>
+
+            <div :id="'collapseMonster'+monsterIndex" class="collapse">
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-4">
+                    <label>
+                      Название
+                      <input class="form-control" type="text" v-model="monsters[monsterIndex].name">
+                    </label>
+                  </div>
+                  <div class="col-4">
+                    <label>
+                      Уровень
+                      <input class="form-control" type="number" v-model="monsters[monsterIndex].level">
+                    </label>
+                  </div>
+                  <div class="col-4">
+                    <label>
+                      Очков здоровья
+                      <input class="form-control" type="number" v-model="monsters[monsterIndex].health">
+                    </label>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-12">
+                    <h4>Описание</h4>
+                    <textarea class="form-control" v-model="monsters[monsterIndex].description"></textarea>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-6">
+                    <h4>Дроп</h4>
+                    <select v-if="objects.length>0" class="form-control" multiple v-model="monsters[monsterIndex].drop">
+                      <option value=""></option>
+                      <option v-for="object in objects" :key="'drop'+object.key"
+                              :value="object.key">
+                        {{ object.name }}
+                      </option>
+                    </select>
+                  </div>
+                  <div class="col-6">
+                    <h4>Предметы для убийства</h4>
+                    <select v-if="objects.length>0" class="form-control" multiple
+                            v-model="monsters[monsterIndex].killObjects">
+                      <option value=""></option>
+                      <option v-for="object in objects" :key="'killObjects'+object.key" :value="object.key">
+                        {{ object.name }}
+                      </option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+
+    <!--    Предметы-->
+    <div class="row">
+      <div class="col-12">
+        <h2 class="d-inline-block">Предметы</h2>
+        <button class="btn btn-success btn-sm" @click="addObject(null)">
+          <i class="fa fa-plus-circle"></i>
+        </button>
+
+        <div class="accordion" id="objectAccordion">
+          <div class="card" v-for="(object, objectIndex) in objects" :key="objectIndex">
+            <div class="card-header" :id="'headingObject'+objectIndex">
+              <h5 class="mb-0">
+                <button class="btn btn-link" type="button" @click="showCollapse('collapseObject'+objectIndex)">
+                  {{ object.name }}
+                </button>
+              </h5>
+            </div>
+
+            <div :id="'collapseObject'+objectIndex" class="collapse">
+              <div class="card-body">
+                <label>
+                  Ключ
+                  <input class="form-control" type="text"
+                         :value="objects[objectIndex].key = getStrHash(objects[objectIndex].name+objects[objectIndex].description+objectIndex)"
+                         readonly>
+                </label>
+                <label>
+                  Название
+                  <input class="form-control" type="text" v-model="objects[objectIndex].name">
+                </label>
+                <label>
+                  Описание
+                  <textarea class="form-control" v-model="objects[objectIndex].description"></textarea>
+                </label>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -86,22 +256,43 @@
 
 export default {
   name: 'Editor',
-  data () {
+  data() {
     return {
       graph: null,
       view: null,
       nodes: [],
+      monsters: [],
+      objects: [],
       renderer: null,
       nodeTemplate: {
         id: 1,
         data: {
-          name: 'item'
+          name: 'item',
+          description: '',
+          resource: {
+            images: [],
+            audio: [],
+            video: []
+          }
         }
+      },
+      monsterTemplate: {
+        name: 'Название монстра',
+        level: 1,
+        health: 100,
+        description: '',
+        drop: [],
+        killObjects: []
+      },
+      objectTemplate: {
+        key: this.getStrHash('Название предмета'),
+        name: 'Название предмета',
+        description: ''
       },
       selectedNode: null
     }
   },
-  created () {
+  created() {
     /* eslint-disable */
     let link = document.createElement("link");
     link.href = "/static/fontawesome-6.4.0/css/all.css";
@@ -115,7 +306,7 @@ export default {
 
     /* eslint-enable */
   },
-  mounted () {
+  mounted() {
     /* eslint-disable */
     let graphics = this.view.svgGraphics()
     graphics.node(function (node) {
@@ -145,25 +336,34 @@ export default {
   },
 
   computed: {
-    linksNodes () {
+    linksNodes() {
       let result = []
       this.nodes.map((node) => {
         result.push({
           id: node.id,
           name: node.data.name,
-          description: '',
-          resource: {
-            images: [],
-            audio: [],
-            video: []
-          }
+          description: node.data.description,
+          resource: node.data.resource
         })
       })
       return result
     }
   },
   methods: {
-    addNode (data = null) {
+    showCollapse(id) {
+      document.getElementById(id).classList.toggle('show')
+    },
+    getStrHash(s) {
+      return s.split('').reduce(function (a, b) {
+        a = ((a << 5) - a) + b.charCodeAt(0)
+        return Math.abs(a & a)
+      }, 0)
+    },
+    /**
+     * Добавить ноду графа
+     * @param data
+     */
+    addNode(data = null) {
       if (data === null) {
         this.nodeTemplate.id++
         this.nodeTemplate.name = 'item ' + this.nodeTemplate.id
@@ -174,22 +374,39 @@ export default {
       let newNode = this.graph.addNode(data.id, data.data)
       this.nodes.push(newNode)
     },
-    addLink (event) {
+    /**
+     * Добавить связь между нодами
+     * @param event
+     */
+    addLink(event) {
       if (this.selectedNode.id !== event.target.value) {
         this.graph.addLink(this.selectedNode.id, event.target.value)
       }
     },
-    updateNode () {
+    /**
+     * Обновить ноду
+     */
+    updateNode() {
       this.graph.addNode(this.selectedNode.id, this.selectedNode.data)
     },
-    removeNode (nodeIndex) {
+    /**
+     * Удалить ноду
+     * @param nodeIndex
+     */
+    removeNode(nodeIndex) {
       this.graph.removeNode(this.nodes[nodeIndex].id)
       this.nodes.splice(nodeIndex, 1)
     },
-    save () {
+    /**
+     * Сохранить граф в локальную память
+     */
+    save() {
       localStorage.setItem('nodes', JSON.stringify(this.nodes))
     },
-    loadSaveNodes () {
+    /**
+     * Загрузить сохраненные данные
+     */
+    loadSaveNodes() {
       let saveNodes = localStorage.getItem('nodes')
       if (saveNodes) {
         try {
@@ -200,11 +417,17 @@ export default {
         }
       }
     },
-    clear () {
+    /**
+     * Очистить ноды
+     */
+    clear() {
       this.nodes = []
       this.graph.clear()
     },
-    exportNodes () {
+    /**
+     * Экспорт графа
+     */
+    exportNodes() {
       const link = document.createElement('a')
       const file = new Blob([JSON.stringify(this.nodes)], {type: 'text/plain'})
       link.href = URL.createObjectURL(file)
@@ -212,7 +435,11 @@ export default {
       link.click()
       URL.revokeObjectURL(link.href)
     },
-    importNodes (nodes) {
+    /**
+     * Импорт нод графа
+     * @param nodes
+     */
+    importNodes(nodes) {
       /**
        * add Nodes
        */
@@ -231,7 +458,11 @@ export default {
         }
       })
     },
-    importExportFile (event) {
+    /**
+     * Импорт файла
+     * @param event
+     */
+    importExportFile(event) {
       let file = event.target.files[0]
       let fr = new FileReader()
       fr.onload = () => {
@@ -245,6 +476,26 @@ export default {
       }
 
       fr.readAsText(file)
+    },
+    /**
+     * Добавить монстра
+     * @param data
+     */
+    addMonster(data = null) {
+      if (data === null) {
+        data = JSON.parse(JSON.stringify(this.monsterTemplate))
+      }
+      this.monsters.push(data)
+    },
+    /**
+     * Добавить предмет
+     * @param data
+     */
+    addObject(data = null) {
+      if (data === null) {
+        data = JSON.parse(JSON.stringify(this.objectTemplate))
+      }
+      this.objects.push(data)
     }
 
   }
