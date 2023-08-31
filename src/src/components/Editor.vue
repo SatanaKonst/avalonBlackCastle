9e33f69e037d44ee05dad9dcc5a15376842245fd
Konstantin <Satana.Konst@gmail.com>
2023-08-31 14:14:49 +0300

    Добавлены контролы в редактор

--- src/src/components/Editor.vue
+++ src/src/components/Editor.vue
@@ -6,7 +6,7 @@
       </div>
       <div class="col-4">
         <h1>Меню управления</h1>
-        <button class="btn btn-info mb-3" type="button" @click="addNode">Добавить локацию</button>
+        <button class="btn btn-info mb-3" type="button" @click="addNode(null)">Добавить локацию</button>
         <div class="row">
           <div class="col-12 mb-5" v-if="selectedNode!==null">
             <h2>Настройки локации</h2>
@@ -23,7 +23,7 @@
             <div class="row">
               <div class="col-12">
                 <h3>Связь</h3>
-                <select class="form-control mb-2" multiple @change="addLink">
+                <select class="form-control mb-2" multiple @change="addLink($event, null)">
                   <option v-for="link in linksNodes"
                           :value="link.id"
                           v-if="link.id!==selectedNode.id"
@@ -32,27 +32,49 @@
                 </select>
               </div>
             </div>
-            <button class="btn btn-success" type="button" @click="updateNode">
+            <button class="btn btn-success" type="button" @click="updateNode()">
               <i class="fa-solid fa-save"> Сохранить</i>
             </button>
           </div>
 
           <div class="col-12">
-            <h2>Локации</h2>
-            <ul class="list-group">
-              <li class="list-group-item" v-for="(node,nodeIndex) in nodes" :key="nodeIndex">
+            <h2 class="d-inline">Локации</h2>
+            <button class="btn btn-sm btn-danger" v-if="nodes.length>0" @click="clear">
+              <i class="fa-solid fa-trash"></i>
+            </button>
+            <ul class="list-group mt-3">
+              <li class="list-group-item"
+                  v-if="nodes && nodes.length>0"
+                  v-for="(node,nodeIndex) in nodes"
+                  :key="nodeIndex">
                 {{ node.data.name }}
                 <button class="btn btn-success float-end" type="button" @click="selectedNode=nodes[nodeIndex]">
                   <i class="fa-solid fa-pen-to-square"></i>
+                </button>
+
+                <button class="btn btn-danger float-end" type="button" @click="removeNode(nodeIndex)">
+                  <i class="fa-solid fa-trash"></i>
                 </button>
               </li>
             </ul>
           </div>
 
-          <div class="col-12" v-if="nodes.length>0">
-            <button class="btn btn-success" type="button" @click="save">
-              <i class="fa-solid fa-save">Сохранить граф</i>
-            </button>
+          <div class="col-12 mt-3">
+            <label v-if="nodes.length===0">
+              Импорт
+              <input class="btn btn-info"
+                     type="file"
+                     @change="importExportFile"
+                     value="Импорт">
+            </label>
+            <div v-if="nodes.length>0">
+              <button class="btn btn-success" type="button" @click="save()">
+                <i class="fa-solid fa-save">Сохранить граф</i>
+              </button>
+              <button class="btn btn-success" type="button" @click="exportNodes()">
+                <i class="fa-solid fa-save">Экспорт</i>
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -64,7 +86,7 @@
 
 export default {
   name: 'Editor',
-  data() {
+  data () {
     return {
       graph: null,
       view: null,
@@ -73,13 +95,13 @@ export default {
       nodeTemplate: {
         id: 1,
         data: {
-          name: 'test'
+          name: 'item'
         }
       },
       selectedNode: null
     }
   },
-  created() {
+  created () {
     /* eslint-disable */
     let link = document.createElement("link");
     link.href = "/static/fontawesome-6.4.0/css/all.css";
@@ -93,7 +115,7 @@ export default {
 
     /* eslint-enable */
   },
-  mounted() {
+  mounted () {
     /* eslint-disable */
     let graphics = this.view.svgGraphics()
     graphics.node(function (node) {
@@ -114,15 +136,16 @@ export default {
       container: document.getElementById('graph'),
       graphics: graphics
     })
-    window.test = this.renderer;
-    window.graph = this.graph;
 
     this.renderer.run()
 
+    this.loadSaveNodes()
+
     /* eslint-enable */
   },
+
   computed: {
-    linksNodes() {
+    linksNodes () {
       let result = []
       this.nodes.map((node) => {
         result.push({
@@ -140,22 +163,88 @@ export default {
     }
   },
   methods: {
-    addNode() {
-      this.nodeTemplate.id++
-      let tmp = JSON.parse(JSON.stringify(this.nodeTemplate))
-      let newNode = this.graph.addNode(tmp.id, tmp.data)
+    addNode (data = null) {
+      if (data === null) {
+        this.nodeTemplate.id++
+        this.nodeTemplate.name = 'item ' + this.nodeTemplate.id
+        data = JSON.parse(JSON.stringify(this.nodeTemplate))
+      } else {
+        this.nodeTemplate.id = data.id
+      }
+      let newNode = this.graph.addNode(data.id, data.data)
       this.nodes.push(newNode)
     },
-    addLink(event) {
+    addLink (event) {
       if (this.selectedNode.id !== event.target.value) {
         this.graph.addLink(this.selectedNode.id, event.target.value)
       }
     },
-    updateNode() {
+    updateNode () {
       this.graph.addNode(this.selectedNode.id, this.selectedNode.data)
     },
-    save() {
+    removeNode (nodeIndex) {
+      this.graph.removeNode(this.nodes[nodeIndex].id)
+      this.nodes.splice(nodeIndex, 1)
+    },
+    save () {
       localStorage.setItem('nodes', JSON.stringify(this.nodes))
+    },
+    loadSaveNodes () {
+      let saveNodes = localStorage.getItem('nodes')
+      if (saveNodes) {
+        try {
+          saveNodes = JSON.parse(saveNodes)
+          this.importNodes(saveNodes)
+        } catch (e) {
+          console.error('Error load saved nodes', e)
+        }
+      }
+    },
+    clear () {
+      this.nodes = []
+      this.graph.clear()
+    },
+    exportNodes () {
+      const link = document.createElement('a')
+      const file = new Blob([JSON.stringify(this.nodes)], {type: 'text/plain'})
+      link.href = URL.createObjectURL(file)
+      link.download = 'graphNodes.txt'
+      link.click()
+      URL.revokeObjectURL(link.href)
+    },
+    importNodes (nodes) {
+      /**
+       * add Nodes
+       */
+      nodes.map((item) => {
+        this.addNode(item)
+      })
+
+      /**
+       * add Links Nodes
+       */
+      nodes.map((item) => {
+        if (item.links && Array.isArray(item.links)) {
+          item.links.map((link) => {
+            this.graph.addLink(link.fromId, link.toId)
+          })
+        }
+      })
+    },
+    importExportFile (event) {
+      let file = event.target.files[0]
+      let fr = new FileReader()
+      fr.onload = () => {
+        try {
+          let tmp = JSON.parse(fr.result)
+          this.importNodes(tmp)
+        } catch (e) {
+          console.error('error parse import file', e)
+          this.nodes = []
+        }
+      }
+
+      fr.readAsText(file)
     }
 
   }
